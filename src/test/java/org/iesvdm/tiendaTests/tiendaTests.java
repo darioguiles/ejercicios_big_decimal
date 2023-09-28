0c8f335674731636f847105d2240e11debe57c58
darioguiles <dario.guilescuesta@hotmail.com>
2023-09-28 13:03:11 +0200

    tiendaOnline

--- src/test/java/org/iesvdm/tiendaTests/tiendaTests.java
+++ src/test/java/org/iesvdm/tiendaTests/tiendaTests.java
@@ -3,7 +3,7 @@ package org.iesvdm.tiendaTests;
 import org.junit.jupiter.api.Test;
 
 public class tiendaTests {
-
+    //Se deben crear clientes y pedidos para que funcionen
     @Test
     public void tiendaFunciona() {
         //Todo
